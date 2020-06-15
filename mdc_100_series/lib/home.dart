f5e13ae69e367754b5c81dc3f8de947c0a7990c8
Pierre Guillen <hi@cerbero.dev>
2020-06-14 22:38:02 -0500

    shrine

---
+++ mdc_100_series/lib/home.dart
@@ -0,0 +1,57 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
+import 'supplemental/asymmetric_view.dart';
+
+class HomePage extends StatelessWidget {
+  // TODO: Add a variable for Category (104)
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.tune),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      body: AsymmetricView(
+          products: ProductsRepository.loadProducts(Category.all)),
+    );
+  }
+}
